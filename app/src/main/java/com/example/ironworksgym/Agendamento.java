82718d4e6eb8e05b4c196d7aeff2642ad7062840
Lucas Emanuel <lucasego050@gmail.com>
2024-09-18 20:15:18 -0300

    project

---
+++ app/src/main/java/com/example/ironworksgym/Agendamento.java
@@ -0,0 +1,17 @@
+package com.example.ironworksgym;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Agendamento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.agendamento);
+
+    }
+
+}
